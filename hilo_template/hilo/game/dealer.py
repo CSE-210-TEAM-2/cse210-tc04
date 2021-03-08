4196e9293a2c0e8f41b4af3ab19ba57ff902f14b
chikaberry <79671470+chikaberry@users.noreply.github.com>
2021-03-08 16:46:30 -0700

    Update

--- hilo_template/hilo/game/dealer.py
+++ hilo_template/hilo/game/dealer.py
@@ -1,5 +1,30 @@
-import random 
+import random
 
+
+
+
+class Dealer:
+def __init__(self):
+
+        """"Class Constructor: will inizialite a dictionary and the num_throws to count
+         the amount of times he is going to throw it"""
+
+        self.cards = []
+        self.num_throws = 0
+        self.points = 300
+
+    def can_draw(self):
+
+        """
+        determines whether or not the Dealer can draw again. 
+        It returns a boolean value that is true if get_points are greater than 0, Otherwise, it returns false.
+        """
+
+        if self.points > 0: 
+            return True
+        
+        else:
+            return False
 #************************ THROW cards **************************
 
     def deal_cards(self):
@@ -45,3 +70,5 @@ import random
         else:
             self.points -= 75
         
+
+
